63e2794d299d880beb7316f6b5f3162dbd56cf7b
Adauto Júnior <adauto.junior@adonitech.com.br>
2025-04-26 16:55:41 -0300

    feat: added the cnpj validation

--- src/index.js
+++ src/index.js
@@ -59,10 +59,52 @@ function validaCPF(cpf) {
          digitoVerificador2 === parseInt(cpf.charAt(10));
 }
 
+/**
+ * Valida se uma string é um CNPJ válido
+ * @param {string} cnpj - CNPJ a ser validado
+ * @returns {boolean} true se for um CNPJ válido, false caso contrário
+ */
+function validaCNPJ(cnpj) {
+  cnpj = cnpj.replace(/[^\d]+/g, '');
+  if (cnpj.length !== 14 || /^(\d)\1{13}$/.test(cnpj)) return false;
+
+  // Validação do primeiro dígito
+  let tamanho = cnpj.length - 2;
+  let numeros = cnpj.substring(0, tamanho);
+  const digitos = cnpj.substring(tamanho);
+  let soma = 0;
+  let pos = tamanho - 7;
+  
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) pos = 9;
+  }
+  
+  let resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado !== parseInt(digitos.charAt(0))) return false;
+  
+  // Validação do segundo dígito
+  tamanho = tamanho + 1;
+  numeros = cnpj.substring(0, tamanho);
+  soma = 0;
+  pos = tamanho - 7;
+  
+  for (let i = tamanho; i >= 1; i--) {
+    soma += numeros.charAt(tamanho - i) * pos--;
+    if (pos < 2) pos = 9;
+  }
+  
+  resultado = soma % 11 < 2 ? 0 : 11 - soma % 11;
+  if (resultado !== parseInt(digitos.charAt(1))) return false;
+  
+  return true;
+}
+
 // Exportando as funções para uso externo
 module.exports = {
   soma,
   subtrai,
   formataMoedaBR,
-  validaCPF
+  validaCPF,
+  validaCNPJ
 };
